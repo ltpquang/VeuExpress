13c960f020aafbe0de8bd0fa59ac3bec316b4270
Quang Le <phuc.quang102@gmail.com>
2014-11-27 20:19:11 +0700

    Initial Commit

---
+++ VeuExpress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VeuExpress
+//
+//  Created by Le Thai Phuc Quang on 11/27/14.
+//  Copyright (c) 2014 Le Thai Phuc Quang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VeuExpress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VeuExpress
+//
+//  Created by Le Thai Phuc Quang on 11/27/14.
+//  Copyright (c) 2014 Le Thai Phuc Quang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
